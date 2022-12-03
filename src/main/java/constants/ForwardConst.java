a0dd06c7d2187a3cac3054f711fa68c497e2d0f5
yuki.mabuchi <mbyuki.0408@gmail.com>
2022-12-03 16:10:43 +0900

    日報editアクションの作成

--- src/main/java/constants/ForwardConst.java
+++ src/main/java/constants/ForwardConst.java
@@ -39,7 +39,7 @@ public enum ForwardConst {
     FW_REP_INDEX("reports/index"),
     FW_REP_SHOW("reports/show"),
     FW_REP_NEW("reports/new"),
-    FW_REP_EDIT("report/edit");
+    FW_REP_EDIT("reports/edit");
 
     /*
      * 文字列
